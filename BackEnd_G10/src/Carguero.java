b2614b72e729a019b6e79cead6ba51b78e88051a
rumanoloko <pasatpetruvlad@gmail.com>
2023-03-24 23:15:11 +0100

    Segunda parte modificada

--- BackEnd_G10/src/Carguero.java
+++ BackEnd_G10/src/Carguero.java
@@ -8,11 +8,24 @@ public class Carguero {
         }
     }
 
+    public boolean Idduplicado(Contenedor c){
+        for(int i=0; i< carguero.length; i++){
+            for(int j=0; j<12; j++){
+                for(int k=9; k>=0; k--){
+                    if(carguero[i].getComplex()[k][j]!=null && carguero[i].getComplex()[k][j].getItentificador()==c.getItentificador())
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public void apilarContenedor(Contenedor c){
         boolean hayHueco = false;
-        switch (c.getPrioridad()){
+        if(!Idduplicado(c) || Idduplicado(c)){
+            switch (c.getPrioridad()){
 
-            case 1:
+                case 1:
 
                     for(int i=0; i<carguero.length; i++){
                         if(carguero[i].getLibresPrioridad1()!=0){
@@ -21,43 +34,61 @@ public class Carguero {
                             break;
                         }
                     }
-                if(!hayHueco)
-                    System.out.println("No caben más contenedores de prioridad 1 en todo el carguero");
-                break;
+                    if(!hayHueco)
+                        System.out.println("No caben más contenedores de prioridad 1 en todo el carguero");
+                    break;
 
-            case 2:
+                case 2:
 
-                for(int i=0; i<carguero.length; i++){
-                    if(carguero[i].getLibresPrioridad2()!=0){
-                        carguero[i].apilarContenedor(c);
-                        hayHueco= !hayHueco;
-                        break;
+                    for(int i=0; i<carguero.length; i++){
+                        if(carguero[i].getLibresPrioridad2()!=0){
+                            carguero[i].apilarContenedor(c);
+                            hayHueco= !hayHueco;
+                            break;
+                        }
                     }
-                }
-                if(!hayHueco)
-                    System.out.println("No caben más contenedores de prioridad 2 en todo el carguero");
-                break;
+                    if(!hayHueco)
+                        System.out.println("No caben más contenedores de prioridad 2 en todo el carguero");
+                    break;
 
-            default:
+                default:
 
-                for(int i=0; i<carguero.length; i++){
-                    if(carguero[i].getLibresPrioridad3()!=0){
-                        carguero[i].apilarContenedor(c);
-                        hayHueco= !hayHueco;
-                        break;
+                    for(int i=0; i<carguero.length; i++){
+                        if(carguero[i].getLibresPrioridad3()!=0){
+                            carguero[i].apilarContenedor(c);
+                            hayHueco= !hayHueco;
+                            break;
+                        }
                     }
-                }
-                if(!hayHueco)
-                    System.out.println("No caben más contenedores de prioridad 3 en todo el carguero");
-                break;
+                    if(!hayHueco)
+                        System.out.println("No caben más contenedores de prioridad 3 en todo el carguero");
+                    break;
+            }
+        } else{
+            System.out.println("El id no es unico para ese contenedor");
         }
 
+    }
+
+    public Contenedor desapilarContenedor(int hub, int columna){
+        Contenedor c;
+        if(hub>0 && hub<4 && columna>0 && columna<13)
+            return carguero[hub-1].desapilarContenedor(columna);
+        return null;
     }
 
     public void mostrarDatosContenedor(int id){
         for(int i=0; i< carguero.length; i++){
             carguero[i].mostrarDatosContenedor(id);
         }
+    }
+
+    public int contenedoresporPais(String pais){
+        int cantidad=0;
+        for(int i=0; i<carguero.length; i++){
+            cantidad += carguero[i].contenedoresPais(pais);
+        }
+        return cantidad;
     }
 
     public String toString(){
--- BackEnd_G10/src/Hub.java
+++ BackEnd_G10/src/Hub.java
@@ -1,143 +1,134 @@
 public class Hub {
 
-    private Contenedor [][] complex;
+    private Contenedor[][] complex;
     private byte libresPrioridad1 = 10, libresPrioridad2 = 10, libresPrioridad3 = 100;
-    public Hub(){
+
+    public Hub() {
         complex = new Contenedor[10][12];
     }
 
-    public void apilarContenedor(Contenedor c){
+    public void apilarContenedor(Contenedor c) {
         boolean error = false;
-        for(int i=0; i<12; i++){
-            if(error) break;
-            for(int j=9; j>=0; j--){
-                if(complex[j][i]==null)
-                    continue;
-                if(complex[j][i].getItentificador()==c.getItentificador())
-                    //error = true;
-                    error = false;
+
+        if (c.getPrioridad() == 1 && complex[0][0] != null)
+            System.out.println("¡ERROR! Ya no caben más contenedores de prioridad 1");
+        if (c.getPrioridad() == 1 && complex[0][0] == null) {
+            for (int i = 9; i >= 0; i--) {
+                if (complex[i][0] == null) {
+                    complex[i][0] = c;
+                    libresPrioridad1--;
                     break;
+                }
             }
         }
-        if(error){
-            System.out.println("¡ERROR! El identificador de ese contenedor debe ser unico");
-        }else{
 
-            if(c.getPrioridad()==1 && complex[0][0]!=null)
-                System.out.println("¡ERROR! Ya no caben más contenedores de prioridad 1");
-            if(c.getPrioridad()==1 && complex[0][0]==null){
-                for(int i=9; i>=0; i--){
-                    if(complex[i][0]==null){
-                        complex[i][0]=c;
-                        libresPrioridad1--;
+        if (c.getPrioridad() == 2 && complex[0][1] != null)
+            System.out.println("¡ERROR! Ya no caben más contenedores de prioridad 2");
+        if (c.getPrioridad() == 2 && complex[0][1] == null) {
+
+            for (int i = 9; i >= 0; i--) {
+                if (complex[i][1] == null) {
+                    complex[i][1] = c;
+                    libresPrioridad2--;
+                    break;
+                }
+            }
+        }
+
+        if (c.getPrioridad() == 3) {
+            boolean hueco = false;
+            for (int i = 2; i < 12; i++) {
+                if (hueco) break;
+                for (int j = 9; j >= 0; j--) {
+                    if (complex[j][i] == null) {
+                        hueco = true;
+                        complex[j][i] = c;
+                        libresPrioridad3--;
                         break;
                     }
                 }
             }
-
-            if(c.getPrioridad()==2 && complex[0][1]!=null)
-                System.out.println("¡ERROR! Ya no caben más contenedores de prioridad 2");
-            if(c.getPrioridad()==2 && complex[0][1]==null){
-
-                for(int i=9; i>=0; i--){
-                    if(complex[i][1]==null){
-                        complex[i][1]=c;
-                        libresPrioridad2--;
-                        break;
-                    }
-                }
-            }
-
-            if(c.getPrioridad()==3){
-                boolean hueco=false;
-                for(int i=2;i<12; i++){
-                    if(hueco) break;
-                    for(int j=9; j>=0; j--){
-                        if(complex[j][i]==null){
-                            hueco = true;
-                            complex[j][i]=c;
-                            libresPrioridad3--;
-                            break;
-                        }
-                    }
-                }
-                if(!hueco) System.out.println("¡ERROR! Ya no caben más contenedores de prioridad 3");
-            }
+            if (!hueco) System.out.println("¡ERROR! Ya no caben más contenedores de prioridad 3");
         }
     }
 
-    public void desapilarContenedor(int columna){
+    public Contenedor desapilarContenedor(int columna) {
 
-        if(columna<1 && columna>12) System.out.println("la columna " + columna + " no existe");
+        Contenedor c = null;
+        if (columna < 1 && columna > 12) System.out.println("la columna " + columna + " no existe");
 
-        if(columna==1 && complex[9][0]==null) System.out.println("No hay contenedores en la columna " + columna + " para desapilar");
-        if(columna==1 && complex[9][0]!=null){
-            for(int i=0; i<10; i++){
-                if(complex[i][0]!=null){
-                    complex[i][0]=null;
+        if (columna == 1 && complex[9][0] == null)
+            System.out.println("No hay contenedores en la columna " + columna + " para desapilar");
+        if (columna == 1 && complex[9][0] != null) {
+            for (int i = 0; i < 10; i++) {
+                if (complex[i][0] != null) {
+                    c = complex[i][0];
+                    complex[i][0] = null;
                     libresPrioridad1++;
                     break;
                 }
             }
         }
 
-        if(columna==2 && complex[9][1]==null)
+        if (columna == 2 && complex[9][1] == null)
             System.out.println("No hay contenedores en la columna " + columna + " para desapilar");
-        if(columna==2 && complex[9][1]!=null){
-            for(int i=0; i<10; i++){
-                if(complex[i][1]!=null){
-                    complex[i][1]=null;
+        if (columna == 2 && complex[9][1] != null) {
+            for (int i = 0; i < 10; i++) {
+                if (complex[i][1] != null) {
+                    c = complex[i][1];
+                    complex[i][1] = null;
                     libresPrioridad2++;
                     break;
                 }
             }
         }
 
-        if(columna>2 && columna<13){
-            boolean vacio=false;
-            if(complex[9][columna-1]==null)
+        if (columna > 2 && columna < 13) {
+            boolean vacio = false;
+            if (complex[9][columna - 1] == null)
                 System.out.println("No hay contenedores en la columna " + columna + " para desapilar");
-            else{
-                for(int i=0; i<10; i++){
+            else {
+                for (int i = 0; i < 10; i++) {
 
-                    if(complex[i][columna-1]!=null){
-                        complex[i][columna-1]=null;
+                    if (complex[i][columna - 1] != null) {
+                        c = complex[i][columna - 1];
+                        complex[i][columna - 1] = null;
                         libresPrioridad3++;
                         break;
                     }
                 }
             }
         }
-
+        return c;
     }
 
-    public String toString(){
-        String s="  ";
-        for(int j=0; j<12; j++) {
-            if(j<9)
-                s += "   " + (j+1);
-            if(j>=9)
-                s += "  " + (j+1);
+    public String toString() {
+        String s = "  ";
+        for (int j = 0; j < 12; j++) {
+            if (j < 9)
+                s += "   " + (j + 1);
+            if (j >= 9)
+                s += "  " + (j + 1);
         }
-        s+= "\n";
+        s += "\n";
 
-        for(int i=0; i<10; i++){
-            if(i<9)
-                s += " " + (i+1) + ": ";
-            if(i==9)
-                s += (i+1) + ": ";
+        for (int i = 0; i < 10; i++) {
+            if (i < 9)
+                s += " " + (i + 1) + ": ";
+            if (i == 9)
+                s += (i + 1) + ": ";
 
 
-            for(int j=0; j<12; j++){
-                if(complex[i][j]==null)
+            for (int j = 0; j < 12; j++) {
+                if (complex[i][j] == null)
                     s += "[ ] ";
-                else{
-                    if(complex[i][j].getPrioridad()==1)
-                        s += "[1] ";
-                    if(complex[i][j].getPrioridad()==2)
-                        s += "[2] ";
-                    if(complex[i][j].getPrioridad()==3)
-                        s += "[3] ";
+                else {
+                    if (complex[i][j].getPrioridad() == 1)
+                        s += "[X] ";
+                    if (complex[i][j].getPrioridad() == 2)
+                        s += "[X] ";
+                    if (complex[i][j].getPrioridad() == 3)
+                        s += "[X] ";
                 }
             }
             s += "\n";
@@ -145,13 +136,13 @@ public class Hub {
         return s;
     }
 
-    public void mostrarDatosContenedor(int id){
-        boolean presente=false;
-        for(int i=0; i<10; i++){
-            for(int j=0; j<12; j++){
+    public void mostrarDatosContenedor(int id) {
+        boolean presente = false;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 12; j++) {
 
-                if(complex[i][j]!=null && complex[i][j].getItentificador()==id){
-                    presente=true;
+                if (complex[i][j] != null && complex[i][j].getItentificador() == id) {
+                    presente = true;
                     System.out.println("Identificador: " + complex[i][j].getItentificador() +
                             "\nPrioridad: " + complex[i][j].getPrioridad() +
                             "\nPeso: " + complex[i][j].getPeso() +
@@ -166,17 +157,21 @@ public class Hub {
         //if(!presente) System.out.println("No existe ningún contenecodor con el identificador " + ide);
     }
 
-    public int contenedoresPais(String país){
-        int cantidad=0;
-        for(int i=0; i<10; i++){
-            for(int j=0; j<12; j++){
-                if(complex[i][j]==null)
+    public int contenedoresPais(String país) {
+        int cantidad = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 12; j++) {
+                if (complex[i][j] == null)
                     continue;
-                if(complex[i][j].getPaísProcedencia().equals(país))
+                if (complex[i][j].getPaísProcedencia().equals(país))
                     cantidad++;
             }
         }
         return cantidad;
+    }
+
+    public Contenedor[][] getComplex() {
+        return complex;
     }
 
     public byte getLibresPrioridad1() {
